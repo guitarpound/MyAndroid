6c40664c28e4d6046bf059b7e093144e0f22d97e
pond <guitarpound@gmail.com>
2020-11-30 22:21:02 +0700

    frist commit

--- app/src/main/java/com/codemobiles/myandroid/MainActivity.kt
+++ app/src/main/java/com/codemobiles/myandroid/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.codemobiles.myandroid
 
+import android.content.ContextWrapper
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.codemobiles.myandroid.databinding.ActivityMainBinding
+import com.pixplicity.easyprefs.library.Prefs
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(packageName)
+                .setUseDefaultSharedPreference(true)
+                .build()
+
+        val token = Prefs.getString("token", "")
+        if (token != "") {
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            setupEventWidget()
+        }
+    }
+
+    private fun setupEventWidget() {
+        binding.loginButton.setOnClickListener {
+            val username =  binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+//            Toast.makeText(applicationContext, "$username ,$password", Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+
+            Prefs.putString("token", "dsfadsfaweoifjllk")
+            Prefs.putString("username", username)
+        }
     }
 }
 
