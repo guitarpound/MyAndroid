b2ffea333bf963e7548d324855897e8e037d5412
pond <guitarpound@gmail.com>
2020-12-01 15:47:13 +0700

    RecyclerView #1

--- app/src/main/java/com/codemobiles/myandroid/JSONFragment.kt
+++ app/src/main/java/com/codemobiles/myandroid/JSONFragment.kt
@@ -6,6 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codemobiles.myandroid.databinding.ActivityMainBinding
+import com.codemobiles.myandroid.databinding.CustomListBinding
+import com.codemobiles.myandroid.databinding.FragmentJsonBinding
 import com.codemobiles.myandroid.models.ProductList
 import com.codemobiles.myandroid.network.NetworkAPI
 import com.codemobiles.myandroid.network.NetworkService
@@ -16,10 +23,23 @@ import retrofit2.Response
 
 class JSONFragment : Fragment() {
 
+    private lateinit var binding: FragmentJsonBinding
+    private lateinit var adapter: CustomAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         feedNetwork()
-        return inflater.inflate(R.layout.fragment_json, container, false)
+
+        binding = FragmentJsonBinding.inflate(inflater, container, false)
+
+        adapter = CustomAdapter()
+        binding.recyclerview.adapter = adapter
+        // important
+//        binding.recyclerview.layoutManager = GridLayoutManager(context, 3)
+//        binding.recyclerview.layoutManager = LinearLayoutManager(context)
+        binding.recyclerview.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
+        return binding.root
     }
 
     private fun feedNetwork() {
@@ -44,5 +64,26 @@ class JSONFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = JSONFragment()
+    }
+
+    inner class CustomAdapter: RecyclerView.Adapter<CustomAdapter.MyViewHolder>(){
+
+        inner class MyViewHolder(val customListBinding: CustomListBinding): RecyclerView.ViewHolder(customListBinding.root) {
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val binding = CustomListBinding.inflate(layoutInflater)
+            return MyViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val binding = holder.customListBinding
+            binding.text1.text = "aaaaa"
+            binding.text2.text = "bbbbb"
+        }
+
+        override fun getItemCount() = 100
+
     }
 }
